60360705edb3e780c0607cc66fab4821ac35846e
ArsFy <arsfy@outlook.com>
2023-01-26 00:51:10 +0800

    del sync transfer_enable & reset transfer_enable

--- app/Console/Commands/ResetTraffic.php
+++ app/Console/Commands/ResetTraffic.php
@@ -117,9 +117,9 @@ class ResetTraffic extends Command
         }
         User::whereIn('id', $users)->update([
             'u' => 0,
-            'd' => 0,
-            'transfer_enable' => User::raw('transfer_plan_enable')
+            'd' => 0
         ]);
+        User::whereIn('id', $users)->update([ 'transfer_enable' => User::raw('transfer_plan_enable') ]);
     }
 
     private function resetByYearFirstDay($builder):void
@@ -127,7 +127,9 @@ class ResetTraffic extends Command
         if ((string)date('md') === '0101') {
             $builder->update([
                 'u' => 0,
-                'd' => 0,
+                'd' => 0
+            ]);
+            $builder->update([
                 'transfer_enable' => User::raw('transfer_plan_enable')
             ]);
         }
@@ -138,7 +140,9 @@ class ResetTraffic extends Command
         if ((string)date('d') === '01') {
             $builder->update([
                 'u' => 0,
-                'd' => 0,
+                'd' => 0
+            ]);
+            $builder->update([
                 'transfer_enable' => User::raw('transfer_plan_enable')
             ]);
         }
@@ -161,8 +165,8 @@ class ResetTraffic extends Command
         }
         User::whereIn('id', $users)->update([
             'u' => 0,
-            'd' => 0,
-            'transfer_enable' => User::raw('transfer_plan_enable')
+            'd' => 0
         ]);
+        User::whereIn('id', $users)->update([ 'transfer_enable' => User::raw('transfer_plan_enable') ]);
     }
 }
--- app/Http/Controllers/Admin/PlanController.php
+++ app/Http/Controllers/Admin/PlanController.php
@@ -44,7 +44,6 @@ class PlanController extends Controller
                 if ($request->input('force_update')) {
                     User::where('plan_id', $plan->id)->update([
                         'group_id' => $params['group_id'],
-                        'transfer_enable' => $params['transfer_enable'] * 1073741824,
                         'transfer_plan_enable' => $params['transfer_enable'] * 1073741824,
                         'speed_limit' => $params['speed_limit']
                     ]);
